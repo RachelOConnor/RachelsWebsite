9ef1f08563ed569bd8a33387b3bff4cf37388a78
unknown <c00284406@setu.ie>
2025-03-14 15:45:37 +0000

    projects page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
+// For dark/light mode
 function changeMode() 
 {
     var element = document.body;
     element.classList.toggle("darkMode");
+
+    if (element.classList.contains("darkMode")) 
+    {
+        localStorage.setItem("darkMode", "enabled");
+    } else 
+    {
+        localStorage.setItem("darkMode", "disabled");
+    }
+
     element.classList.add("fadeAnimation");
 
-    setTimeout(function(){
+    setTimeout(function()
+    {
         element.classList.remove("fadeAnimation");
     }, 1000);
+}
+
+
+
+// For page transitions
+document.addEventListener('DOMContentLoaded', function(event) 
+{
+    if (localStorage.getItem("darkMode") === "enabled") 
+    {
+        document.body.classList.add("darkMode");
+    }
+    document.body.style.opacity = 1;
+})
+
+
+window.transitionToPage = function(href) 
+{
+    document.body.style.opacity = 0;
+    setTimeout(function() 
+    { 
+        window.location.href = href
+    }, 500)
 }
