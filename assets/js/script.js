ed590005ec910f7699397a76ddf47862e00cc00f
unknown <c00284406@setu.ie>
2025-03-14 13:28:40 +0000

    home page complete?

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ function changeMode()
 {
     var element = document.body;
     element.classList.toggle("darkMode");
+    element.classList.add("fadeAnimation");
+
+    setTimeout(function(){
+        element.classList.remove("fadeAnimation");
+    }, 1000);
 }
